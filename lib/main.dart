99a69ed23092316129b9b94e66bf7902c4b96740
chaniru-puldith <a.a.d.chaniru.p@gmail.com>
2023-08-07 18:34:28 +0530

    Play sound when button clicked

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +20,8 @@ class XyloApp extends StatelessWidget {
                   backgroundColor: Colors.red,
                 ),
                 onPressed: () {
-                  print('button clicked');
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note1.wav'));
                 },
                 child: const Text(''),
               ),
